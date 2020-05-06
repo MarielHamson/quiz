4833c5f698c98629d2f7c8ad6a2b3f2431146912
MarielHamson <mariel.hamson@gmail.com>
2020-05-06 16:21:49 -0700

    debug submit and change button color

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 
 // Business logic
 
-var add = function(number1, number2, number3) {
-	return number1 + number2 + number3;
+var add = function(number1, number2, number3, number4, number5) {
+	return number1 + number2 + number3 + number4 + number5;
 };
 
 // Interface logic
@@ -14,8 +14,10 @@ $(document).ready(function() {
     var number1 = parseInt($("input:radio[name=answer1]:checked").val());
     var number2 = parseInt($("input:radio[name=answer2]:checked").val());
     var number3 = parseInt($("input:radio[name=answer3]:checked").val());
-    var result = add(number1, number2, number3);
-
+    var number4 = parseInt($("input:radio[name=answer4]:checked").val());
+    var number5 = parseInt($("input:radio[name=answer5]:checked").val());
+    var result = add(number1, number2, number3, number4, number5);
+    
     if (result >= 5 && result <= 8) {
       alert("You should study Ruby");
     } else if (result >=9 && result <= 12) {
